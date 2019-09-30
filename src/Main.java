2ff2ed09a26a3fdc3898038b5e24f7e9c6c195e4
ZenMoore <1057398161@qq.com>
2019-09-30 19:02:37 +0800

    get segout and statout

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,40 @@
-import org.apdplat.word.WordSegmenter;
-import org.apdplat.word.segmentation.Word;
+import org.apdplat.word.WordFrequencyStatistics;
+import org.apdplat.word.segmentation.SegmentationAlgorithm;
 
-import java.util.List;
+import java.io.File;
 
 public class Main {
 
+    private Main() {
+    }
+
     public static void main(String[] args) {
-        List<Word> words = WordSegmenter.seg("郭晓珂在四川大学");
-        System.out.println(words);
+        //遍历文件夹中所有txt
+        //对每个txt分析词频
+        //将词频统计结果存放在平行文件夹中
+        //词频统计设置
+        WordFrequencyStatistics wordFrequencyStatistics = new WordFrequencyStatistics();
+        wordFrequencyStatistics.setRemoveStopWord(false);
+//        wordFrequencyStatistics.setResultPath("word-frequency-statistics.txt");
+        wordFrequencyStatistics.setSegmentationAlgorithm(SegmentationAlgorithm.MaxNgramScore);
+
+        String path = args[0];//data\input
+        String segoutpath = args[1];//data\segout
+        String statoutpath = args[2];//data\statout
+
+        File[] files = new File(path).listFiles();
+        if (new File(statoutpath).mkdir()) {
+            for (File f : files) {
+                try {
+                    wordFrequencyStatistics.seg(f, new File(segoutpath + File.separator + "segresults_" + f.getName()));
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
+                wordFrequencyStatistics.dump(statoutpath + File.separator + "statresults_" + f.getName());
+            }
+        } else {
+            System.out.println("没有相应的词频输出文件夹或者该文件夹已存在，需要删除后重新运行这个程序。");
+        }
     }
 }
